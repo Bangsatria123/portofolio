3a57e9c96ed989b925be228b3cbbaf35a338da0c
Your Name <you@example.com>
2024-08-15 16:56:09 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <section className=" h-screen flex justify-center items-center">
+      <h1 className="text-2xl p-2 font-bold">test</h1>
+    </section>
+  );
+}
+
+export default App;
